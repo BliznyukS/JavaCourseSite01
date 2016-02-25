96e66144021b7913bf48c51438d8f0bcde2ad284
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-25 21:36:35 +0200

    Java Course Site

---
+++ src/LoopWhileFour.java
@@ -0,0 +1,20 @@
+public class LoopWhileFour {
+    public static void main(String[] args) {
+        int count = 4;
+
+        int i = 0;
+
+        while (i < count) {
+
+            int k= 0;
+            while (k++ < i+1) {
+
+                System.out.print(" *");
+            }
+
+            System.out.println();
+            i++;
+        }
+
+    }
+}
