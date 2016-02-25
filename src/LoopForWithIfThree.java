4ec5c967283142db38bc5812bca9c5ead6aa6387
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-25 22:39:56 +0200

    Java Course Site

--- src/LoopForWithIfThree.java
+++ src/LoopForWithIfThree.java
@@ -4,13 +4,14 @@ public class LoopForWithIfThree {
 
         // Внешний цикл
         for (int i = 0; i < count; i++) {
-            for (int k = 0; k < i + 1; k++) {
+            for (int k = 0; k < count; k++) {
                 // Здесь условие даже немного проще
-                if (k == 0 || i == 0 || k == i || i == count - 1) {
-                    System.out.print("*");
+                if (i == count - 1 || k == count - 1) {
+                    System.out.print(" *");
                 } else {
                     System.out.print(" ");
                 }
+
             }
             // Переход на следующую строку
             System.out.println();
